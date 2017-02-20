7b38cab4a755c1fbb578ebade361b5d9c8f4bd7b
Anthony Orio <orioa9@students.rowan.edu>
2017-02-19 22:16:06 -0500

    Linked the .fxml file for the GUI and created a controller class so that we can begin adding functionality to the components.

---
+++ src/Controller.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by Anthony Orio on 2/19/2017.
+ */
+public class Controller {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@
  * 2-13-17
  */
 
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +17,10 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
+        primaryStage.setTitle("ROM Database");
+        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.show();
     }
 }
