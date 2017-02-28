bfa99131fa554d77dc7514f3975e0152106024a3
Anthony Orio <orioa9@students.rowan.edu>
2017-02-27 19:56:38 -0500

    Recreated the error checking method. Added double data fields to store all of the text field data.

--- src/ROMdb/Controllers/EstimationController.java
+++ src/ROMdb/Controllers/EstimationController.java
@@ -9,8 +9,16 @@ import javafx.scene.layout.Pane;
 
 import javax.swing.*;
 import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EstimationController {
+
+    private final String path = "";
+
+    private double  staffDay, staffMonth, cprs, integrationWeight,
+                    unitTestingWeight, codeWeight, defaultSlocs,
+                    designWeight, budgetUpgrade, budgetMaint, ddrCwtSlocs;
 
     @FXML private Pane estimationBase;
 
@@ -26,15 +34,16 @@ public class EstimationController {
     @FXML private Label label_designWeight;
     @FXML private Label label_codeWeight;
     @FXML private Label label_unitTestingWeight;
-    @FXML private Label label_intergrationWeight;
+    @FXML private Label label_integrationWeight;
     @FXML private Label label_defaultSlocs;
 
     @FXML private ComboBox<?> combo_estimateBaseline;
 
+
     @FXML private TextField field_staffDay;
     @FXML private TextField field_staffMonth;
     @FXML private TextField field_cprs;
-    @FXML private TextField field_intergrationWeight;
+    @FXML private TextField field_integrationWeight;
     @FXML private TextField field_unitTestingWeight;
     @FXML private TextField field_codeWeight;
     @FXML private TextField field_defaultSlocs;
@@ -45,92 +54,161 @@ public class EstimationController {
     @FXML private TextField field_budgetMaint;
     @FXML private TextField field_ddrCwtSlocs;
 
-    @FXML private Button button_esimateSubmit;
+    @FXML private Button button_estimateSubmit;
 
     public void test1() {
         System.out.println("Test");
     }
 
+
     @FXML
     public void errorChecking() {
         String error = "";
-        try {
-            error = "SLOCs/Staff Month";
-            Double.parseDouble(field_staffMonth.getText());
-            error = "SLOCs/Staff-Day";
-            Double.parseDouble(field_staffDay.getText());
-            error = "DDR/CWT SLOCS";
-            Double.parseDouble(field_ddrCwtSlocs.getText());
-            error = "Integration Weight";
-            Double.parseDouble(field_intergrationWeight.getText());
-            error = "Unit Test Weight";
-            Double.parseDouble(field_unitTestingWeight.getText());
-            error = "Code Weight";
-            Double.parseDouble(field_codeWeight.getText());
-            error = "Default SLOCS";
-            Double.parseDouble(field_defaultSlocs.getText());
-            error = "Design Weight";
-            Double.parseDouble(field_designWeight.getText());
-            error = "Budget Upgrade";
-            Double.parseDouble(field_budgetUpgrade.getText());
-            error = "Budget Maintenance";
-            Double.parseDouble(field_budgetMaint.getText());
+        String parsed = "";
+        String pattern = "\\d+(\\.\\d{1,25})?";
+        boolean errorExists = false;
+
+
+        parsed = field_staffMonth.getText();
+        if (parsed.matches(pattern)) {
+            this.staffMonth = Double.parseDouble(parsed);
+        } else {
+            error += "SLOCs/Staff Month\n";
+            errorExists = true;
         }
-        catch (Exception e) {
-            error = "You've entered the wrong input for: " + error;
+
+        parsed = field_cprs.getText();
+        if (parsed.matches(pattern)) {
+            this.cprs = Double.parseDouble(parsed);
+        } else {
+            error += "CPRS#\n";
+            errorExists = true;
+        }
+
+        parsed = field_integrationWeight.getText();
+        if (parsed.matches(pattern)) {
+            this.integrationWeight = Double.parseDouble(parsed);
+        } else {
+            error += "Integration Weight\n";
+            errorExists = true;
+        }
+
+        parsed = field_unitTestingWeight.getText();
+        if (parsed.matches(pattern)) {
+            this.unitTestingWeight = Double.parseDouble(parsed);
+        } else {
+            error += "Unit Testing Weight\n";
+            errorExists = true;
+        }
+
+        parsed = field_codeWeight.getText();
+        if (parsed.matches(pattern)) {
+            this.codeWeight = Double.parseDouble(parsed);
+        } else {
+            error += "Code Weight\n";
+            errorExists = true;
+        }
+
+        parsed = field_defaultSlocs.getText();
+        if (parsed.matches(pattern)) {
+            this.defaultSlocs = Double.parseDouble(parsed);
+        } else {
+            error += "Default SLOCS\n";
+            errorExists = true;
+        }
+
+        parsed = field_designWeight.getText();
+        if (parsed.matches(pattern)) {
+            this.designWeight = Double.parseDouble(parsed);
+        } else {
+            error += "Design Weight\n";
+            errorExists = true;
+        }
+
+        parsed = field_budgetUpgrade.getText();
+        if (parsed.matches(pattern)) {
+            this.budgetUpgrade = Double.parseDouble(parsed);
+        } else {
+            error += "Budget Upgrade\n";
+            errorExists = true;
+        }
+
+        parsed = field_budgetMaint.getText();
+        if (parsed.matches(pattern)) {
+            this.budgetMaint = Double.parseDouble(parsed);
+        } else {
+            error += "Budget Maintenance\n";
+            errorExists = true;
+        }
+
+        parsed = field_ddrCwtSlocs.getText();
+        if (parsed.matches(pattern)) {
+            this.ddrCwtSlocs = Double.parseDouble(parsed);
+        } else {
+            error += "DDR CWR SLOCS\n";
+            errorExists = true;
+        }
+
+        if (errorExists) {
+            error = "You've entered the wrong input for: \n" + error;
             JOptionPane.showMessageDialog(null, error);
-            return;
         }
 
-
-        if(     (Double.parseDouble(field_designWeight.getText()) + Double.parseDouble(field_codeWeight.getText()) +
-                Double.parseDouble(field_intergrationWeight.getText()) + Double.parseDouble(field_unitTestingWeight.getText())) != 100  )
-        {
+        if( (designWeight + codeWeight + integrationWeight + unitTestingWeight) != 100) {
             error = "Your weights do not add up to 100";
             JOptionPane.showMessageDialog(null, error);
         }
-
-        /** If they want empty text fields to be checked...
-         *
-        if(         field_budgetMaint.getText().equals("") || field_designWeight.getText().equals("") || field_designWeight.getText().equals("")
-                || field_designWeight.getText().equals("") || field_designWeight.getText().equals("") || field_designWeight.getText().equals("")
-                || field_designWeight.getText().equals("") || field_designWeight.getText().equals("") || field_designWeight.getText().equals("")) {
-
-        }
-        */
     }
+
+
 
     //          String s = "          Hello World                    ";
     // ---->     s.trim()
     // ---->     s = "Hello World"
     public void writeToDB() {
-        try
-        {
 
-            //Connection conn = DriverManager.getConnection(
-            //            "jdbc:ucanaccess://TestDatabase.accdb");
 
-            Connection conn = DriverManager.getConnection(
-                    "jdbc:ucanaccess://C://Users//Anthony Orio//Desktop//Rowan//Software Engineering//Project//Gorillas-DB-Redesign//src//ROMdb//rom_dcti Update 2012 Rev 1.mdb");
-
-            String query = "SELECT * FROM basicrom";
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(query);
-
-            while(rs.next()) // Retrieve data from ResultSet
-            {
-                System.out.print("Serial number : "+ rs.getString(1)); //1st column of Table from database
-                System.out.print(" , Name : "+ rs.getString(2)); //2nd column of Table
-                System.out.print(" and Age : "+ rs.getString(3)); //4th column of Table
-                System.out.println(" , City : "+ rs.getString(4)); //3rd column of Table
-
-            }
-            //s.close();
-            conn.close();
-        }
-        catch (Exception e)
-        {
-            System.out.println("Exception : "+ e);
-        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        try
+//        {
+//            //Connection conn = DriverManager.getConnection(
+//            //            "jdbc:ucanaccess://TestDatabase.accdb");
+//
+//            Connection conn = DriverManager.getConnection(
+//                    "jdbc:ucanaccess://C://Users//Anthony Orio//Desktop//Rowan//Software Engineering//Project//Gorillas-DB-Redesign//src//ROMdb//rom_dcti Update 2012 Rev 1.mdb");
+//
+//            String query = "SELECT * FROM basicrom";
+//            Statement st = conn.createStatement();
+//            ResultSet rs = st.executeQuery(query);
+//
+//            while(rs.next()) // Retrieve data from ResultSet
+//            {
+//                System.out.print("Serial number : "+ rs.getString(1)); //1st column of Table from database
+//                System.out.print(" , Name : "+ rs.getString(2)); //2nd column of Table
+//                System.out.print(" and Age : "+ rs.getString(3)); //4th column of Table
+//                System.out.println(" , City : "+ rs.getString(4)); //3rd column of Table
+//
+//            }
+//            //s.close();
+//            conn.close();
+//        }
+//        catch (Exception e)
+//        {
+//            System.out.println("Exception : "+ e);
+//        }
