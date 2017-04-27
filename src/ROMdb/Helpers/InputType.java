d603a4c41593159f516c1efa20a2e48f03118a6c
Chris McKane <Chrisrmckane@gmail.com>
2017-04-27 17:48:47 -0400

    Slight change to the look of the system.

--- src/ROMdb/Helpers/InputType.java
+++ src/ROMdb/Helpers/InputType.java
@@ -11,7 +11,7 @@ public enum InputType
      */
     ALPHA("^[a-zA-Z]*$", "Alpha"),
     ALPHA_SPACE("^[a-zA-Z\\s]*$", "Alpha Space"),
-    ALPHA_NUMERIC("^[a-zA-Z0-9]*$", "Alpha Numeric"),
+    ALPHA_NUMERIC("^[a-zA-Z0-9\\.\\-]*$", "Alpha Numeric"),
     ALPHA_NUMERIC_SPACE("^[a-zA-Z0-9\\s]*$", "Alpha Numeric Space"),
     DOUBLE("\\d+(\\.\\d{1,25})?", "Double"),
     EMPTY_STRING("^$", "Empty String"),
